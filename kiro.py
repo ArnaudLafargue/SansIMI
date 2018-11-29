ca71226dec7e111d3aa23223ef948da73c02873e
ArnaudLafargue <arnaudlafargue@orange.fr>
2018-11-29 19:13:54 +0100

    Update kiro.py

--- kiro.py
+++ kiro.py
@@ -1,6 +1,7 @@
 from typing import Tuple, TextIO
 import numpy as np
 import random as ran
+from operator import itemgetter, attrgetter, methodcaller
 
 nodes_file = open("grenoble/nodes.csv")
 distances_file = open("grenoble/distances.csv")
@@ -45,7 +46,7 @@ class Instance:
         terminal = []
         distribution = []
         i = 0
-        
+
         for i in range(len(self.nodes)):
             if self.nodes[i][2] == 't':
                 terminal.append([i, float(self.nodes[i][0]), float(self.nodes[i][1])])
@@ -58,73 +59,68 @@ class Instance:
 
     def list_distribution(self):
         return self.nodes_sorting()[1]
-    
+
 
 I = Instance()
 T = [i for i in I.terminal]
 
+
 def binom(I):
     terminal = I.terminal
     binome = []
-    while len(terminal)>1:
-        i = ran.randint(0,len(terminal)-1)
+    while len(terminal) > 1:
+        i = ran.randint(0, len(terminal) - 1)
         min = 1000000000000
-        j0=0
+        j0 = 0
         for j in range(len(terminal)):
-            if (i!=j):
-                if I.l(terminal[i][0],terminal[j][0]) +I.l(terminal[j][0],terminal[i][0])< min :
-                    j0 =j
-                    min = I.l(terminal[i][0],terminal[j][0]) +I.l(terminal[j][0],terminal[i][0])
-        if j0 <i :
-            binome.append ([terminal.pop(i)[0], terminal.pop(j0)[0]])
-        if j0 >i:
-            binome.append ([terminal.pop(i)[0], terminal.pop(j0-1)[0]])
-    return (binome,terminal)
+            if (i != j):
+                if I.l(terminal[i][0], terminal[j][0]) + I.l(terminal[j][0], terminal[i][0]) < min:
+                    j0 = j
+                    min = I.l(terminal[i][0], terminal[j][0]) + I.l(terminal[j][0], terminal[i][0])
+        if j0 < i:
+            binome.append([terminal.pop(i)[0], terminal.pop(j0)[0]])
+        if j0 > i:
+            binome.append([terminal.pop(i)[0], terminal.pop(j0 - 1)[0]])
+    return (binome, terminal)
 
 
-        
 def quadrunome(I):
     bino = binom(I)
     binome = bino[0]
     terminal = bino[1]
     quadrunome = []
-    while len(binome)>1:
-        i = ran.randint(0,len(binome)-1)
+    while len(binome) > 1:
+        i = ran.randint(0, len(binome) - 1)
         inf = 1000000000000
-        j0=0
+        j0 = 0
         for j in range(len(binome)):
-            if (i!=j):
-                if min((I.l(binome[i][0],binome[j][0]) +I.l(binome[j][0],binome[i][0])  ,I.l(binome[i][0],binome[j][1])+I.l(binome[j][1],binome[i][0]),I.l(binome[i][1],binome[j][0])+I.l(binome[j][0],binome[i][1]),I.l(binome[i][1],binome[j][1])+I.l(binome[j][1],binome[i][1]))) < inf :
-                    j0 =j
-                    inf = min((I.l(binome[i][0],binome[j][0]) +I.l(binome[j][0],binome[i][0])  ,I.l(binome[i][0],binome[j][1])+I.l(binome[j][1],binome[i][0]),I.l(binome[i][1],binome[j][0])+I.l(binome[j][0],binome[i][1]),I.l(binome[i][1],binome[j][1])+I.l(binome[j][1],binome[i][1])))
-        if j0 <i :
+            if (i != j):
+                if min((I.l(binome[i][0], binome[j][0]) + I.l(binome[j][0], binome[i][0]),
+                        I.l(binome[i][0], binome[j][1]) + I.l(binome[j][1], binome[i][0]),
+                        I.l(binome[i][1], binome[j][0]) + I.l(binome[j][0], binome[i][1]),
+                        I.l(binome[i][1], binome[j][1]) + I.l(binome[j][1], binome[i][1]))) < inf:
+                    j0 = j
+                    inf = min((I.l(binome[i][0], binome[j][0]) + I.l(binome[j][0], binome[i][0]),
+                               I.l(binome[i][0], binome[j][1]) + I.l(binome[j][1], binome[i][0]),
+                               I.l(binome[i][1], binome[j][0]) + I.l(binome[j][0], binome[i][1]),
+                               I.l(binome[i][1], binome[j][1]) + I.l(binome[j][1], binome[i][1])))
+        if j0 < i:
             var = binome.pop(i)
             var2 = binome.pop(j0)
-            quadrunome.append ([var[0],var[1], var2[0],var2[1]])
-        if j0 >i:
+            quadrunome.append([var[0], var[1], var2[0], var2[1]])
+        if j0 > i:
             var = binome.pop(i)
-            var2 = binome.pop(j0-1)
-            quadrunome.append ([var[0],var[1], var2[0],var2[1]])
-                       
-               
-        compteur = 0        
-    if (len(binome)>0):
+            var2 = binome.pop(j0 - 1)
+            quadrunome.append([var[0], var[1], var2[0], var2[1]])
+
+    if (len(binome) > 0):
         a = binome[0][0]
         b = binome[0][1]
-        compteur = 2
-    if len(binome)>0:
+    if len(binome) > 0:
         c = terminal[0][0]
-        if compteur == 2 :
-            compteur = 3
-        if compteur == 0 :
-            compteur = 1
-    if compteur == 1:
-        quadrunome.append([c])
-    if compteur == 2: 
-        quadrunome.append([a,b])
-    if compteur == 3:
-        quadrunome.append([a,b,c])
+    quadrunome.append([a, b, c])
     return quadrunome
+
 
 Q = quadrunome(I)
 
@@ -136,8 +132,17 @@ class Antenna_group():
     def dist(self, t_antenna):
         return I.l(self.representative,t_antenna)+ I.l(t_antenna,self.representative)
 
+
 Antenna_groups = [Antenna_group(i) for i in range(len(Q))]
 
-d = I.distribution[0][0]
+D = I.distribution
+for i in range(len(D)):
+    d = D[i][0]
+    group = []
+    distances = []
+    for j in range(len(Antenna_groups)):
+        distances += [(Antenna_groups[j].dist(d),j)]
+    sorted_distances = sorted(distances, key=itemgetter(0))
+    print(sorted_distances)
 
-print(Antenna_groups[1].dist(d))
+
