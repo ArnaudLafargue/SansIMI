83dc435ac6a498da9d70792417964c6d9ff17245
ArnaudLafargue <arnaudlafargue@orange.fr>
2019-01-05 18:33:59 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include "recuit.h"
 
-int main()
+int main1()
 {
     parser Parser;
 
@@ -16,4 +16,36 @@ int main()
     cout << finalcost << "final"<<endl;
 
     return 0;
+}
+
+int main2()
+{
+    parser Parser;
+
+    instances Instances(Parser);
+//    int finalcost =0;
+//    solution bestsol;
+//    for (int n=0;n<size_distribution;n++){
+//        recuit Recuit;
+//        (Recuit.heuristique(Parser,Instances.rangement[n])).copy(bestsol);
+//        bestsol.fichier(Parser,Instances.rangement[n]);
+//        finalcost +=bestsol.cout_non_incremental(Parser,Instances.rangement[n]);
+//    }
+//    cout << finalcost << "final"<<endl;
+
+    solutions sols;
+    sols = solutions_parser();
+
+    return 0;
+}
+
+int main3(){
+    solutions sols;
+    sols = solutions_parser();
+    return 0;
+}
+
+
+int main(){
+    return main2();
 }
